97fe570aa60df7f7a1a328e1ba1f7f04dc5d0d06
James Smith <james@Jamess-MacBook-Air.local>
2019-02-04 22:18:58 -0700

    added create room form

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,14 +3,27 @@ import * as firebase from 'firebase';
 import './../App.js';
 
 
-class RoomList extends Component {
+export default class RoomList extends Component {
   constructor(props) {
     super(props);
       this.state = {
+        name: "",
         rooms: []
       };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ name: this.state.name });
+    this.setState({ name: "" });
   }
 
   componentDidMount() {
@@ -21,15 +34,25 @@ class RoomList extends Component {
     });
   }
 
+
+
   render() {
+    const roomForm = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.name} placeholder="Enter Room Name" onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+
     const roomList = this.state.rooms.map((room) =>
-      <li key={room.key}>{room.name}</li>
+      <li className="room-name" key={room.key}>{room.name}</li>
     );
 
     return(
-      <ul>{roomList}</ul>
+      <div className="chat-rooms">
+        <div className="room-form">{roomForm}</div>
+        <ul className="room-list">{roomList}</ul>
+      </div>
     );
   }
 }
-
-export default RoomList;
